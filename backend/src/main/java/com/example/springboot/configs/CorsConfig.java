29c626c665c3d9f9b8f098bf27a64b16bc4b17d3
Khady Gaye <yacinekhady809@gmail.com>
2026-01-05 20:33:44 +0100

    configure cors

--- backend/src/main/java/com/example/springboot/configs/CorsConfig.java
+++ backend/src/main/java/com/example/springboot/configs/CorsConfig.java
@@ -1,4 +1,18 @@
 package com.example.springboot.configs;
 
-public class CorsConfig {
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "portfolio-a8w.pages.dev"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
